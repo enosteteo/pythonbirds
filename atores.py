db33054ac57d74be2c29e92522e907bc5cf7b61b
Enos Tetéo <enosfrancisco@gmail.com>
2020-05-10 23:33:03 -0300

    Atores - 8/8 OK

--- atores.py
+++ atores.py
@@ -38,7 +38,8 @@ class Ator():
         :param tempo: o tempo do jogo
         :return: posição x, y do ator
         """
-        return 1, 1
+
+        return self.x, self.y
 
     def colidir(self, outro_ator, intervalo=1):
         """
@@ -52,11 +53,12 @@ class Ator():
         :param intervalo: Intervalo a ser considerado
         :return:
         """
-        # self.x += 1
-        # self.y += 1
-        # if self.y == outro_ator.y:
-        #     if self.status()
-        pass
+        if self.status == ATIVO and outro_ator.status == ATIVO:
+            delta_x = abs(self.x - outro_ator.x)
+            delta_y = abs(self.y - outro_ator.y)
+            if delta_x <= intervalo and delta_y <= intervalo:
+                self.status = outro_ator.status = DESTRUIDO
+
 
 
 class Obstaculo(Ator):
